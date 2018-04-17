e3e7ce025f12f7fc550da9077830671c97b96157
Tom Deseyn <tom.deseyn@gmail.com>
2018-04-17 15:51:12 +0200

    Print request headers to Console

--- Startup.cs
+++ Startup.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OpenshiftTestApp
 {
+    public static class IApplicationBuilderExtensions
+    {
+        public static void LogRequestHeaders(this IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                var builder = new StringBuilder(Environment.NewLine);
+                foreach (var header in context.Request.Headers)
+                {
+                    builder.AppendLine($"{header.Key}:{header.Value}");
+                }
+                System.Console.WriteLine(builder.ToString());
+                await next.Invoke();
+            });
+        }
+    }
+
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -20,6 +39,7 @@ namespace OpenshiftTestApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.LogRequestHeaders();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
